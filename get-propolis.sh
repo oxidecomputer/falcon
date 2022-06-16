c904db62d69b0a6fe8708e496a8232cfdbf4e7c8
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-06-16 15:46:30 -0700

    update get-propolis.sh to match recent dependency updates

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01FX23JYFGRT6WWEBWM7AQ6P8S/FVHQObKPFHxbcKX9myal6ipg31Wozuv4VBU5pRHalj5T6tsU/01FX23K7SZQANV8116B3HVTA2J/01FX247YHE5AART13CNZN888QM/propolis-server
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01G5J04PA2HXWMPFM0XKQHV0Q4/j6JtstnpNbsdh2LA3sfjcvC7RQBEktw4FTleVe0gF6FaPM6M/01G5J04Y2P3X8JHQ9BJ4DQTR12/01G5J0YA93SB3V7RZYH0JW60FA/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
