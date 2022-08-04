e42d2bbae25ca587f99372a806d1ba24785e1ad0
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-08-03 23:47:13 -0700

    add config commands for dendrite/dpd

--- examples/softnpu/cargo-bay/router-configure.sh
+++ examples/softnpu/cargo-bay/router-configure.sh
@@ -6,6 +6,6 @@ chmod +x routeradm
 ./routeradm add-address fe80::aae1:deff:fe01:701d
 ./routeradm add-address fe80::aae1:deff:fe01:701e
 
-./routeradm add-route fd00:3000:: 24 3
 ./routeradm add-route fd00:1000:: 24 1
 ./routeradm add-route fd00:2000:: 24 2
+./routeradm add-route fd00:3000:: 24 3
