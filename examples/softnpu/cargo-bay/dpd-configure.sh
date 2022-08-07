10d1025a364f3bae3f1240f6da47c24ac35701a3
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-08-07 12:18:06 -0700

    remove sleeps in routeradm/dpd configure scripts

--- examples/softnpu/cargo-bay/dpd-configure.sh
+++ examples/softnpu/cargo-bay/dpd-configure.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 ./swadm dp port set 1:0 ipv6 fe80::aae1:deff:fe01:701c
-sleep 2
 ./swadm dp port set 2:0 ipv6 fe80::aae1:deff:fe01:701d
-sleep 2
 ./swadm dp port set 3:0 ipv6 fe80::aae1:deff:fe01:701e
-sleep 2
 
 ./swadm dp route add fd00:1000::/24 1:0
-sleep 2
 ./swadm dp route add fd00:2000::/24 2:0
-sleep 2
 ./swadm dp route add fd00:3000::/24 3:0
--- examples/softnpu/cargo-bay/router-configure.sh
+++ examples/softnpu/cargo-bay/router-configure.sh
@@ -3,14 +3,9 @@
 chmod +x routeradm
 
 ./routeradm add-address fe80::aae1:deff:fe01:701c
-sleep 2
 ./routeradm add-address fe80::aae1:deff:fe01:701d
-sleep 2
 ./routeradm add-address fe80::aae1:deff:fe01:701e
-sleep 2
 
 ./routeradm add-route fd00:1000:: 24 1
-sleep 2
 ./routeradm add-route fd00:2000:: 24 2
-sleep 2
 ./routeradm add-route fd00:3000:: 24 3
