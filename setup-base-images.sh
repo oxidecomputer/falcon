fc3826f64bd5294db8aa7861417630cb3e505700
Levon Tarver <levon@oxide.computer>
2022-08-18 22:07:36 +0000

    BUGFIX: extract correct image to zpool for netstack

--- setup-base-images.sh
+++ setup-base-images.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 set -e
+set -o xtrace
 
 dataset=${FALCON_DATASET:-rpool/falcon}
+echo "dataset is: $dataset"
 
 # Images follow the naming scheme
 #   <name>-<os_version>-<image_version>
@@ -23,6 +25,7 @@ for img in $images; do
     file=$img.raw.xz
     if [[ ! -f $file ]]; then
         echo "Pulling $file"
+        echo "https://oxide-falcon-assets.s3.us-west-2.amazonaws.com/$file"
         curl -OL https://oxide-falcon-assets.s3.us-west-2.amazonaws.com/$file
     fi
     if [[ ! -f $img.raw ]]; then
@@ -60,7 +63,7 @@ function fetch_and_verify {
         echo "latest $1 archive present"
     else
         echo "latest $1 archive is not present, or it is corrupted"
-        echo "fetching latest $1 archive"
+        echo "fetching latest $1 archive from $IMAGE_URL"
         curl -OL $IMAGE_URL
         sha256sum --status -c "$1.sha256"
     fi
@@ -114,8 +117,8 @@ name=${VERSION%_*}
 if [[ ! -b /dev/zvol/dsk/$dataset/img/$name ]]; then
     echo "Creating ZFS volume $name"
     pfexec zfs create -p -V 20G "$dataset/img/$name"
-    echo "Copying contents of image into volume"
-    pfexec dd if=$img.raw of="/dev/zvol/dsk/$dataset/img/$name" conv=sync
+    echo "Copying contents of image $VERSION into volume"
+    pfexec dd if=$VERSION.raw of="/dev/zvol/dsk/$dataset/img/$name" conv=sync
     echo "Creating base image snapshot"
     pfexec zfs snapshot "$dataset/img/$name@base"
 else
