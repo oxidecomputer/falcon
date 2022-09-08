c0258cc11d0c72912ee71f2911d640a7c0408251
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-09-07 20:08:54 -0700

    gateway nat config, bumper rails for softnpu tty

--- examples/softnpu/cargo-bay/gateway-init.sh
+++ examples/softnpu/cargo-bay/gateway-init.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 ipadm create-addr -t -T static -a 10.100.0.1/24 vioif0/v4
+
+echo "map vioif1 10.100.0.0/24 -> 0/32 portmap tcp/udp 1025:65000" > /etc/ipf/ipnat.conf
+echo "map vioif1 10.100.0.0/24 -> 0/32 portmap" >> /etc/ipf/ipnat.conf
+
+pfexec ipf -E
+routeadm -e ipv4-forwarding -u
+svcadm enable -s ipfilter
+
+pfexec ipnat -f /etc/ipf/ipnat.conf
--- examples/softnpu/cargo-bay/router-configure.sh
+++ examples/softnpu/cargo-bay/router-configure.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
+set -x
+
 chmod +x softnpuadm
 
 ./softnpuadm add-address6 fe80::aae1:deff:fe01:701c
-sleep 0.25
+sleep 1
 ./softnpuadm add-address6 fe80::aae1:deff:fe01:701d
-sleep 0.25
+sleep 1
 ./softnpuadm add-address6 fe80::aae1:deff:fe01:701e
-sleep 0.25
-./softnpuadm add-address6 fe00:99::1
-sleep 0.25
+sleep 1
+./softnpuadm add-address6 fd00:99::1
+sleep 1
 
 ./softnpuadm add-route6 fd00:1000:: 24 1 fe80::aae1:deff:fe00:1
-sleep 0.25
+sleep 1
 ./softnpuadm add-route6 fd00:2000:: 24 2 fe80::aae1:deff:fe00:2
-sleep 0.25
+sleep 1
 ./softnpuadm add-route6 fd00:3000:: 24 3 fe80::aae1:deff:fe00:3
-sleep 0.25
-./softnpuadm add-route4 10.100.0.0  24 4 10.100.0.5
-sleep 0.25
+sleep 1
+./softnpuadm add-route4 1.1.1.1  24 4 10.100.0.1
+sleep 1
 
 ./softnpuadm add-ndp-entry fe80::aae1:deff:fe00:1 a8:e1:de:00:00:01
-sleep 0.25
+sleep 1
 ./softnpuadm add-ndp-entry fe80::aae1:deff:fe00:2 a8:e1:de:00:00:02
-sleep 0.25
+sleep 1
 ./softnpuadm add-ndp-entry fe80::aae1:deff:fe00:3 a8:e1:de:00:00:03
-sleep 0.25
-./softnpuadm add-arp-entry 10.100.0.5 a8:e1:de:00:00:04
-sleep 0.25
+sleep 1
+./softnpuadm add-arp-entry 10.100.0.1 a8:e1:de:00:00:04
+sleep 1
 
 ./softnpuadm add-nat4 10.100.0.5 4000 7000 fd00:2000::1 
