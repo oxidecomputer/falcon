10e6344854ff4b48780a7ca73d55bd9972b4faec
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-09-14 22:53:42 -0700

    boundary services nat entries for the strings

--- examples/softnpu/cargo-bay/router-configure.sh
+++ examples/softnpu/cargo-bay/router-configure.sh
@@ -19,4 +19,9 @@ chmod +x softnpuadm
 ./softnpuadm add-ndp-entry fe80::aae1:deff:fe00:3 a8:e1:de:00:00:03
 ./softnpuadm add-arp-entry 10.100.0.1 a8:e1:de:00:00:04
 
+#piano
 ./softnpuadm add-nat4 10.100.0.5 4000 7000 fd00:2000::1 10 A8:40:25:ff:00:01
+#violin
+./softnpuadm add-nat4 10.100.0.5 1000 2000 fd00:1000::1 10 A8:40:25:ff:00:02
+#cello
+./softnpuadm add-nat4 10.100.0.5 2001 3000 fd00:3000::1 10 A8:40:25:ff:00:03
