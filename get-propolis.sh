d8080746526cef18ad3da83c58cfcf570d9ba987
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-12-07 12:52:40 -0800

    update propolis instructions and scripts (#22)

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GBPKTP9Z2S98VN5J8PQK3V3J/Od78nXJ73d8K0LCUnsenvEZT6ezITMBJ1NlFEMVAgPaeopZU/01GBPKTYBXCHNKM5ZFJXSCZTEY/01GBPMXNGVXY31FH0MKZD9GHTH/propolis-server
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GKAGZWTYQMNZVZYQNH99N5F2/14N8NTF7oj4b25oLM2oSGyGj102Ciqo4VV7kcminJPmCQHir/01GKAH052JACY454V95JGC6W8G/01GKAJ08FE9KVX4G64259ENPQT/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
