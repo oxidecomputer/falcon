fe1b5c33f8cfaad168f7fc7ff3b0ad806f142d84
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-08-07 16:14:14 -0700

    move to helios 2 (#39)

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GSF17F77NAEHA3C168HGX7Y2/SAqcSrBk1e8myW5zB7qv7sxcksBHhZqF7rYFwDO5gbNok6N8/01GSF17Q60TKDZ7AY6ZVVKQGBJ/01GSF273KX3GBQMZKR8PQF6ZF0/propolis-server
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01H6ZF04VHZ3FPXWVKZ08JNE32/Hhe40QjvN0RczlpuS4WQLbbYcerlnJC2yvwMzKqKnuGLjFSK/01H6ZF0FGJN6KCF7GYT19W4J6H/01H6ZFRVBK0E4D54ZSQ5AGT4WD/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
