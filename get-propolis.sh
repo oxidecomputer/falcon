689e77ab7d1913fd96512d80acf267d65c55375c
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-04-30 12:08:30 -0700

    bump propolis, add raw image import script (#71)
    * bump propolis, add raw image import script
    
    * bump propolis crate version

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01HFAFE96NCPZJNBF6PW3PP986/yitxU6XK2dA309oP5UrNkFOpgDZeFK3IiooWQgF8oCdV8qbY/01HFAFET8B2SFM7NDB4K53AFAT/01HFAG8SG6266CSKHMJ8W676YK/propolis-server
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01HWNQE2GN1HF0FZ9TB3FW45EA/gQ0fn20VcNgnMykIfnhD6GdXx4eeiEny46kZTY9pHLJvcluk/01HWNQEEMJNB86YGC83Z0FG2WE/01HWNR2ARSN1QJG1NGP9S60XCN/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
---
+++ import-raw-img.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -x
+
+dataset=${FALCON_DATASET:-rpool/falcon}
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: import-raw-img <file> <name>"
+    exit 1
+fi
+
+file=$1
+name=$2
+
+if [[ $FORCE == 1 ]]; then
+    echo "Deleting $name image"
+    pfexec zfs destroy -r $dataset/img/$name || true
+fi
+if [[ ! -b /dev/zvol/dsk/$dataset/img/$name ]]; then
+    echo "Creating ZFS volume $name"
+    fsize=`stat --format "%s" $file`
+    let vsize=(fsize + 4096 - size%4096)
+    pfexec zfs create -p -V $vsize -o volblocksize=4k "$dataset/img/$name"
+    echo "Copying contents of image into volume"
+    pfexec dd if=$file of="/dev/zvol/rdsk/$dataset/img/$name" bs=1024k status=progress
+    echo "Creating base image snapshot"
+    pfexec zfs snapshot "$dataset/img/$name@base"
+fi
