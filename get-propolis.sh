8eb440eabf29741982243d58c598013a074d2f43
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-05-01 07:55:37 -0700

    bump propolis

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01HWNQE2GN1HF0FZ9TB3FW45EA/gQ0fn20VcNgnMykIfnhD6GdXx4eeiEny46kZTY9pHLJvcluk/01HWNQEEMJNB86YGC83Z0FG2WE/01HWNR2ARSN1QJG1NGP9S60XCN/propolis-server
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01HWT9E3HTD7M58MD1VJP8ZZ09/a5obopISYhQaVlQ4PX8dfGWIowWs51drDMzVkTK2DBoo2D0I/01HWT9EP3DVZGG1RW0QYTXS5DY/01HWTA6K1RQWGH45ECTGFJJ3E1/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
