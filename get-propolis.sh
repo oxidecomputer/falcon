51436a266270f86c3b880ffc29f36249637d2db3
Elaine <167589650+elaine-oxide@users.noreply.github.com>
2024-08-02 12:59:13 -0700

    Update buildomat artefact for propolis-server

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01HWT9E3HTD7M58MD1VJP8ZZ09/a5obopISYhQaVlQ4PX8dfGWIowWs51drDMzVkTK2DBoo2D0I/01HWT9EP3DVZGG1RW0QYTXS5DY/01HWTA6K1RQWGH45ECTGFJJ3E1/propolis-server
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01J47SP5WG406KHSV43K8TBVVA/Mm53OBbS1Seynk2zC2N6BFMdCRUVzr8t66s78CGOIbFBpqFa/01J47SPHH2ST71530E4YT9EP3Z/01J47TB8PB1FG0A299WCNP1B04/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
