17bcdbc4754a46323a7217a6ebb0abaa7c98bd13
Zeeshan Lakhani <zeeshan@oxidecomputer.com>
2024-09-03 13:40:30 -0400

    [deps] update to newest propolis with an api change (#90)

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL  https://buildomat.eng.oxide.computer/wg/0/artefact/01J4QN90YQBKK0B095P1KESX47/V4c9mEBv79HtOsmXeCz4C1m4dQBDdzB2ESHAxvA3UdZIdG4O/01J4QN9CV8X92EBANRYWVMNZR2/01J4QNXRHZQBSJZFXQDE2Y2JJB/propolis-server
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01J6W7HNVVWNXZKE0GFDC3MDG3/d6LnQXh0v9uk19YRRZzTG9ILNJD32nQe51QgzSQKCDdDZ2za/01J6W7J5HNW26WFAV7M6KYD43F/01J6W87ECGKJGAXDQFPYZ7JMJ4/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
