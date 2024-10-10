651fb5889d66be90ac1afa4a730c573b643aef1e
artemis everfree <artemis@oxidecomputer.com>
2024-10-10 14:56:16 -0700

    image import script fixups (#95)
    - only download images if we're actually going to install them ( resolve #94 )
    - change vol size calculation syntax to allow shellcheck to check it
    - fix a minor logic bug in zvol size calculation from using a non-existent variable (thanks shellcheck!)
    - add `-o nounset` to make using non-existent variables an error
    - update FORCE check to work in the presence of `-o nounset`
      - Note I've done it in a way that changes behavior so `FORCE=<any string>` will force. `FORCE=1`, `FORCE=y`, `FORCE=jabberwocky`, etc.
    - add `-o pipefail` out of habit
    
    I haven't actually executed `import-raw-img.sh`, just ported changes over as I made them to `setup-base-images.sh`. I have tested `setup-base-images.sh` though.

--- import-raw-img.sh
+++ import-raw-img.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-set -e
-set -x
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
 
 dataset=${FALCON_DATASET:-rpool/falcon}
 
@@ -13,14 +15,14 @@ fi
 file=$1
 name=$2
 
-if [[ $FORCE == 1 ]]; then
+if [[ -n "${FORCE+x}" ]]; then
     echo "Deleting $name image"
     pfexec zfs destroy -r $dataset/img/$name || true
 fi
 if [[ ! -b /dev/zvol/dsk/$dataset/img/$name ]]; then
     echo "Creating ZFS volume $name"
     fsize=`stat --format "%s" $file`
-    let vsize=(fsize + 4096 - size%4096)
+    (( vsize = fsize + 4096 - ( fsize % 4096 ) ))
     pfexec zfs create -p -V $vsize -o volblocksize=4k "$dataset/img/$name"
     echo "Copying contents of image into volume"
     pfexec dd if=$file of="/dev/zvol/rdsk/$dataset/img/$name" bs=1024k status=progress
