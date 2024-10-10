d4291b9143f1ced5d6332554de9027222decdc44
Artemis Everfree <artemis@oxidecomputer.com>
2024-10-10 13:25:02 -0700

    only dl if needed; fix `size` typo -> `fsize`

--- setup-base-images.sh
+++ setup-base-images.sh
@@ -20,7 +20,14 @@ for img in $images; do
     if [[ $FORCE == 1 ]]; then
         rm -f $file
         rm -rf $img.raw
+        echo "Deleting $name image"
+        pfexec zfs destroy -r $dataset/img/$name || true
     fi
+
+    if [[ -b /dev/zvol/dsk/$dataset/img/$name ]]; then
+        continue
+    fi
+
     if [[ ! -f $file ]]; then
         echo "Pulling $file"
         echo "https://oxide-falcon-assets.s3.us-west-2.amazonaws.com/$file"
@@ -32,22 +39,15 @@ for img in $images; do
     fi
     file=$img.raw
 
-
     name=${img%_*}
-    if [[ $FORCE == 1 ]]; then
-        echo "Deleting $name image"
-        pfexec zfs destroy -r $dataset/img/$name || true
-    fi
-    if [[ ! -b /dev/zvol/dsk/$dataset/img/$name ]]; then
-        echo "Creating ZFS volume $name"
-        fsize=`stat --format "%s" $img.raw`
-        let vsize=(fsize + 4096 - size%4096)
-        pfexec zfs create -p -V $vsize -o volblocksize=4k "$dataset/img/$name"
-        echo "Copying contents of image into volume"
-        pfexec dd if=$img.raw of="/dev/zvol/rdsk/$dataset/img/$name" bs=1024k status=progress
-        echo "Creating base image snapshot"
-        pfexec zfs snapshot "$dataset/img/$name@base"
-    fi
+    echo "Creating ZFS volume $name"
+    fsize=`stat --format "%s" $img.raw`
+    let vsize=$((fsize + 4096 - ( fsize % 4096 ) ))
+    pfexec zfs create -p -V $vsize -o volblocksize=4k "$dataset/img/$name"
+    echo "Copying contents of image into volume"
+    pfexec dd if=$img.raw of="/dev/zvol/rdsk/$dataset/img/$name" bs=1024k status=progress
+    echo "Creating base image snapshot"
+    pfexec zfs snapshot "$dataset/img/$name@base"
 done
 
 popd
