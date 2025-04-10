ccc61d52f203b317985773a95c43cc51452e0142
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2025-04-10 07:55:55 -0700

    use buildomat artifact to download propolis

--- get-propolis.sh
+++ get-propolis.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# from propolis' `falcon` job, commit ba4d338f0c98faa0328502c99aa6fa0e0948d3da
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01JQA00H3HQ2H7JW0PWMDQ7XDN/52rIztYQlpRJEttYOMaflSu2b8GnmLPUnW6TsjkChj37dvgp/01JQA00TDFWX2870391FPKBY76/01JQA0N556K971D0XGQPGJYVKJ/propolis-server
+# get propolis version from Cargo.toml
+rev=`cat Cargo.toml | grep propolis | sed -E 's/.*rev = "([^"]+)".*/\1/'`
+echo "Fetching propolis version $rev"
+
+curl -OL https://buildomat.eng.oxide.computer/public/file/oxidecomputer/propolis/falcon/$rev/propolis-server
 chmod +x propolis-server
 pfexec mv propolis-server /usr/bin/
